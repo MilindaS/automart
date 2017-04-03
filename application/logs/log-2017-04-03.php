92c4bfab123b7e2dd15e0e52368e4d49c7ee55ba
HLSMilinda <hlsmilinda@gmail.com>
2017-04-03 07:08:02 +0530

    Fixing up

---
+++ application/logs/log-2017-04-03.php
@@ -0,0 +1,45 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2017-04-03 07:05:36 --> Config Class Initialized
+DEBUG - 2017-04-03 07:05:36 --> Hooks Class Initialized
+DEBUG - 2017-04-03 07:05:36 --> Utf8 Class Initialized
+DEBUG - 2017-04-03 07:05:36 --> UTF-8 Support Enabled
+DEBUG - 2017-04-03 07:05:36 --> URI Class Initialized
+DEBUG - 2017-04-03 07:05:36 --> Router Class Initialized
+DEBUG - 2017-04-03 07:05:36 --> No URI present. Default controller set.
+DEBUG - 2017-04-03 07:05:36 --> Output Class Initialized
+DEBUG - 2017-04-03 07:05:36 --> Security Class Initialized
+DEBUG - 2017-04-03 07:05:36 --> Input Class Initialized
+DEBUG - 2017-04-03 07:05:36 --> Global POST and COOKIE data sanitized
+DEBUG - 2017-04-03 07:05:36 --> Language Class Initialized
+DEBUG - 2017-04-03 07:05:36 --> Language Class Initialized
+DEBUG - 2017-04-03 07:05:36 --> Config Class Initialized
+DEBUG - 2017-04-03 07:05:36 --> Loader Class Initialized
+DEBUG - 2017-04-03 07:05:36 --> Helper loaded: url_helper
+DEBUG - 2017-04-03 07:05:36 --> Database Driver Class Initialized
+DEBUG - 2017-04-03 07:05:36 --> Session Class Initialized
+DEBUG - 2017-04-03 07:05:36 --> Helper loaded: string_helper
+DEBUG - 2017-04-03 07:05:36 --> A session cookie was not found.
+DEBUG - 2017-04-03 07:05:36 --> Session routines successfully run
+DEBUG - 2017-04-03 07:05:36 --> Controller Class Initialized
+DEBUG - 2017-04-03 07:05:36 --> Home MX_Controller Initialized
+DEBUG - 2017-04-03 07:05:36 --> File loaded: application/controllers/../modules/item/controllers/item.php
+DEBUG - 2017-04-03 07:05:36 --> Item MX_Controller Initialized
+DEBUG - 2017-04-03 07:05:36 --> Model Class Initialized
+DEBUG - 2017-04-03 07:05:36 --> File loaded: application/modules/item/models/mdl_item.php
+DEBUG - 2017-04-03 07:05:36 --> Model Class Initialized
+DEBUG - 2017-04-03 07:05:36 --> File loaded: application/controllers/../modules/tracking/controllers/tracking.php
+DEBUG - 2017-04-03 07:05:36 --> Tracking MX_Controller Initialized
+DEBUG - 2017-04-03 07:05:36 --> File loaded: application/modules/tracking/models/mdl_tracking.php
+DEBUG - 2017-04-03 07:05:36 --> Model Class Initialized
+DEBUG - 2017-04-03 07:05:36 --> File loaded: application/modules/home/views/metaHeader.php
+DEBUG - 2017-04-03 07:05:36 --> File loaded: application/modules/home/views/header.php
+DEBUG - 2017-04-03 07:05:36 --> File loaded: application/modules/home/views/cdTopLinkBar.php
+DEBUG - 2017-04-03 07:05:36 --> File loaded: application/controllers/../modules/category/controllers/category.php
+DEBUG - 2017-04-03 07:05:36 --> Category MX_Controller Initialized
+DEBUG - 2017-04-03 07:05:36 --> File loaded: application/modules/category/models/mdl_category.php
+DEBUG - 2017-04-03 07:05:36 --> Model Class Initialized
+DEBUG - 2017-04-03 07:05:36 --> File loaded: application/modules/home/views/home.php
+DEBUG - 2017-04-03 07:05:36 --> File loaded: application/modules/home/views/footer.php
+DEBUG - 2017-04-03 07:05:36 --> Final output sent to browser
+DEBUG - 2017-04-03 07:05:36 --> Total execution time: 0.0709
